bf6f26b12af0bc1393741cfa13e093e9964a796a
hunter9713 <dip288ghosh@gmail.com>
2026-08-23 12:01:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,37 @@ function copyCode() {
     }, 1500);
 
 }
+
+int led13 = 13;
+int led11 = 11;
+int led9 = 9;
+
+void setup() {
+
+    pinMode(led13, OUTPUT);
+    pinMode(led11, OUTPUT);
+    pinMode(led9, OUTPUT);
+
+}
+
+void loop() {
+
+    digitalWrite(led13, HIGH);
+    delay(10000);
+
+    digitalWrite(led13, LOW);
+    delay(30);
+
+    digitalWrite(led11, HIGH);
+    delay(15000);
+
+    digitalWrite(led11, LOW);
+    delay(25);
+
+    digitalWrite(led9, HIGH);
+    delay(15000);
+
+    digitalWrite(led9, LOW);
+    delay(25);
+
+}
